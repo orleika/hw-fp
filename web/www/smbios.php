6717d7bbe3c96ecbaf42b549cabc100b4db95698
orleika <admin@orleika.io>
2017-11-20 16:05:44 +0900

    Support non-dockernized envrironment

--- web/www/smbios.php
+++ web/www/smbios.php
@@ -1,4 +1,13 @@
 <?php
+require_once dirname(__FILE__) . '/vendor/autoload.php';
+$dotenv = new Dotenv\Dotenv(__DIR__ . '../');
+$dotenv->load();
+
+$_ENV['MYSQL_HOST'] = getenv('MYSQL_HOST');
+$_ENV['MYSQL_DATABASE'] = getenv('MYSQL_DATABASE');
+$_ENV['MYSQL_USER'] = getenv('MYSQL_USER');
+$_ENV['MYSQL_PASSWORD'] = getenv('MYSQL_PASSWORD');
+
 $dsn = "mysql:host={$_ENV['MYSQL_HOST']};dbname={$_ENV['MYSQL_DATABASE']};charset=utf8";
 $user = $_ENV['MYSQL_USER'];
 $password = $_ENV['MYSQL_PASSWORD'];
